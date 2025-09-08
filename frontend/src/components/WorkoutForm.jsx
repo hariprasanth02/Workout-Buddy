93cf3e25d6dc5bf493a62b58fe2be8800d663dd0
hariprasanth02 <prasanthbandla03@gmail.com>
2025-09-08 10:17:05 +0530

    used useReducer hook

--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
+import { useWorkoutsContext } from '../hooks/useWorkoutContext'
 
 const WorkoutForm = () => {
+    const {dispatch} = useWorkoutsContext()
     const [title,setTitle] = useState('')
     const [load,setLoad] = useState('')
     const [reps,setReps] = useState('')
@@ -21,10 +23,11 @@ const WorkoutForm = () => {
             setError(json.error)
         }else{
             setError(null);
-        setTitle('')
-        setLoad('')
-        setReps('')
-        console.log("New Workout Added:",json)
+            setTitle('')
+            setLoad('')
+            setReps('')
+            console.log("New Workout Added:",json)
+            dispatch({type:'CREATE_WORKOUT',payload:json})
 
         }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,22 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import WorkoutDetails from '../components/WorkoutDetails'
 import WorkoutForm from '../components/WorkoutForm'
+import { useWorkoutsContext } from '../hooks/useWorkoutContext'
 
 WorkoutDetails
+useWorkoutsContext
 
 const Home = () => {
-  const [workouts,setWorkout] = useState(null)
+  // const [workouts,setWorkout] = useState(null)
+  const {workouts,dispatch} = useWorkoutsContext()
   useEffect(()=>{
     const fetchWorkouts = async ()=>{
       const response = await fetch('/api/workouts/')
       const json = await response.json()
 
       if(response.ok){
-        setWorkout(json)
+        // setWorkout(json)
+        dispatch({type:'SET_WORKOUTS',payload:json})
       }
     }
     fetchWorkouts()
-  },[])
+  },[dispatch])
   return (
     <div className='home'>
      <div className='workouts'>
