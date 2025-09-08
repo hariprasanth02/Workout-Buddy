d5fc3ef02c1d4964b67c6f823c1b0d87ba57d13d
hariprasanth02 <prasanthbandla03@gmail.com>
2025-09-08 19:54:30 +0530

    login and logout pages created

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 const Navbar = () => {
+  const {logout} = useLogout()
+  const handleClick = () =>{
+    logout()
+
+  }
+
   return (
    <header>
     <div className='container'>
@@ -10,6 +17,9 @@ const Navbar = () => {
         </Link>
       <nav>
         <div>
+          <button onClick={handleClick}>Log Out</button>
+        </div>
+        <div> 
           <Link to={"/login"}>Login</Link>
           <Link to={"/signup"}>Sign Up</Link>
         </div>
