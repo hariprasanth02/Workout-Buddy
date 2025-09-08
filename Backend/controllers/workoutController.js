5fda88a345f4f886b18607e8c8fa3408c369fe89
hariprasanth02 <prasanthbandla03@gmail.com>
2025-09-08 23:37:34 +0530

    All auth routes compled

--- Backend/controllers/workoutController.js
+++ Backend/controllers/workoutController.js
@@ -2,7 +2,8 @@ const Workout = require('../models/workoutModel')
 const mongoose = require('mongoose')
 
 exports.getWorkouts = async(req,res)=>{
-    const workouts = await Workout.find({}).sort({createdAt:-1})
+    const user_id = req.user._id
+    const workouts = await Workout.find({user_id}).sort({createdAt:-1})
     if(!workouts)
         return res.status(400).json({error:"No entries found"})
     res.status(200).json(workouts)
@@ -20,9 +21,25 @@ exports.getWorkout = async(req,res)=>{
 }
 
 exports.createWorkout = async(req,res)=>{
+    
     const {title,load,reps} = req.body;
+    let emptyFields = [];
+    if(!title){
+        emptyFields.push('title')
+    }if(!load){
+        emptyFields.push('load')
+    }if(!reps){
+        emptyFields.push('reps')
+    }
+
+    if(emptyFields.length >0){
+        return res.status(400).json({error:'Please fill out all the fields!',emptyFields})
+
+    }
+
     try{
-        const workout = await Workout.create({title,load,reps})
+        const user_id = req.user._id
+        const workout = await Workout.create({title,load,reps,user_id})
         res.status(200).json(workout)
     }catch(error){
         res.status(400).json({error:error.message})
--- frontend/src/hooks/useLogout.jsx
+++ frontend/src/hooks/useLogout.jsx
@@ -1,12 +1,16 @@
+
 import {useAuthContext} from './useAuthContext'
+import {useWorkoutsContext} from "./useWorkoutContext"
 
 export const useLogout = ()=>{
     const {dispatch} = useAuthContext()
+    const {dispatch:workoutsDispatch} = useWorkoutsContext()
 
     const logout = ()=>{
         localStorage.removeItem('user')
 
         dispatch({type:'LOGOUT'})
+        workoutsDispatch({type:'SET_WORKOUTS',payload:null})
     }
     return{logout}
 }
